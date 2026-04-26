ec54d97790f4994435ef9e0d4c89c1154833b7f4
Cyber69t3dprint <yudiel83@gmail.com>
2026-04-26 04:39:08 +0200

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cyber69-cache-v4';
+const CACHE_NAME = 'cyber69-cache-v5';
 const urlsToCache = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', event => {
